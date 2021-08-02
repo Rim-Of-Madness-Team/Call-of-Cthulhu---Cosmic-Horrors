79e11b817bf75348a484d7490300a1fe0f024c56
Matthew Walls <matt.walls31@gmail.com>
2021-08-01 20:55:01 -0700

    Star Vampire Spawner Bugfix
    Fixed a bug where the Star Vampire would not spawn and had no AI behavior

--- Source/CosmicHorrors/IncidentWorker_StarVampireAttack.cs
+++ Source/CosmicHorrors/IncidentWorker_StarVampireAttack.cs
@@ -1,143 +1,65 @@
-﻿///Always required
-///Possible libraries
-using RimWorld;
+﻿using RimWorld;
+using System;
+using System.Linq;
+using UnityEngine;
 using Verse;
 using Verse.Sound;
-using Verse.AI.Group;
-//using RimWorld.Planet;
-//using Verse.AI;
-//using Verse.AI.Group;
-
 
 namespace CosmicHorror
 {
-    public class IncidentWorker_StarVampireAttack : IncidentWorker
-    {
-        private CosmicHorrorPawn iwVampire;   //The Star Vampire Pawn
-        private ThingDef iwDef;           //For the custom Spawner from JecsTools
-        private Faction iwFac;                //The Star Vampire Faction
-        private IntVec3 iwLoc;                //The Star Vampire location
-        private SoundDef iwWarn;              //The Star Vampire Warning Noise
-        private Lord lord;                    //The Star Vampire AI manager
+	public class IncidentWorker_StarVampireAttack : IncidentWorker
+	{
+		protected override bool CanFireNowSub(IncidentParms parms)
+		{
+			if (!base.CanFireNowSub(parms))
+			{
+				return false;
+			}
+			Map map = (Map)parms.target;
+			IntVec3 intVec;
+			return RCellFinder.TryFindRandomPawnEntryCell(out intVec, map, CellFinder.EdgeRoadChance_Animal, false, null);
+		}
 
-        protected override bool CanFireNowSub(IncidentParms parms)
+		private int NumberToSpawn(IncidentParms parms)
         {
-            if (GenDate.DaysPassed < (ModInfo.cosmicHorrorRaidDelay + this.def.earliestDay))
-            {
-                Log.Message("Cosmic Horrors :: CantFireDueTo Time :: " + GenDate.DaysPassed + " days passed, but we need " + ModInfo.cosmicHorrorRaidDelay.ToString() + " days + " + this.def.earliestDay);
-                return false;
-            }
-            return true;
-        }
+			Map iwMap = (Map)parms.target;
 
-        protected override bool TryExecuteWorker(IncidentParms parms)
-        {
+			int iwCount = 1;
 
-            //Resolve parameters.
-            ResolveSpawnCenter(parms);
+			if (parms.points <= 2000f)
+			{
+				iwCount = 1;
+			}
+			else if (parms.points <= 3000f)
+			{
+				iwCount = 2;
+			}
+			else
+			{
+				iwCount = 3;
+			}
+			return iwCount;
+		}
 
-            //Initialize variables.
-            this.iwDef        = ResolveSpawner(parms);
-            this.iwWarn       = MonsterDefOf.Pawn_ROM_StarVampire_Warning;
-            this.iwVampire    = null; //iwPawn as CosmicHorrorPawn;
-            this.iwLoc        = CellFinder.RandomClosewalkCellNear(parms.spawnCenter, (Map)parms.target, 8);
+		protected override bool TryExecuteWorker(IncidentParms parms)
+		{
+			Map map = (Map)parms.target;
+			IntVec3 intVec;
+			if (!RCellFinder.TryFindRandomPawnEntryCell(out intVec, map, CellFinder.EdgeRoadChance_Animal, false, null))
+			{
+				return false;
+			}
+			map.GetComponent<MapComponent_StarVampireTracker>().AddNewStarVampireSpawner(intVec, NumberToSpawn(parms));
 
-            //In-case there's something silly happening...
-            if (this.iwFac == null)
-            {
-                this.iwFac = Find.FactionManager.FirstFactionOfDef(FactionDefOf.AncientsHostile);
-            }
+			//Slow down time
+			Find.TickManager.slower.SignalForceNormalSpeed();
+			//Play a sound.
+			MonsterDefOf.Pawn_ROM_StarVampire_Warning.PlayOneShotOnCamera();
+			//Show the warning message.
+			Messages.Message("StarVampireIncidentMessage".Translate(), new RimWorld.Planet.GlobalTargetInfo(IntVec3.Invalid, (Map)parms.target), MessageTypeDefOf.SituationResolved);
 
-            //Slow down time
-            Find.TickManager.slower.SignalForceNormalSpeed();
-            //Play a sound.
-            this.iwWarn.PlayOneShotOnCamera();
-            //Show the warning message.
-            Messages.Message("StarVampireIncidentMessage".Translate(), new RimWorld.Planet.GlobalTargetInfo(IntVec3.Invalid, (Map)parms.target), MessageTypeDefOf.SituationResolved);
-            //Spawn the Star Vampire.
-            SpawnStarVampires(parms);
-            return true;
-        }
+			return true;
+		}
 
-        private static ThingDef ResolveSpawner(IncidentParms parms)
-        {
-            Map iwMap = (Map)parms.target;
-            var abberation = (GenCelestial.IsDaytime(GenCelestial.CelestialSunGlow(iwMap, Find.TickManager.TicksAbs)))
-                ? MonsterDefOf.ROM_StarVampireSpawnerAbberation
-                : MonsterDefOf.ROM_StarVampireSpawnerNight;
-
-            var chance = Rand.Value;
-            if (chance > 0.3)
-                return MonsterDefOf.ROM_StarVampireSpawner;   
-            else if (chance > 0.05)
-                return abberation;
-            else
-                return MonsterDefOf.ROM_StarVampireSpawnerAlbino;
-        }
-
-        /// <summary>
-        /// Where to, Cthulhu?
-        /// </summary>
-        /// <param name="parms"></param>
-        protected void ResolveSpawnCenter(IncidentParms parms)
-        {
-            Map iwMap = (Map)parms.target;
-            if (parms.spawnCenter.IsValid)
-            {
-                return;
-            }
-            if (Rand.Value < 0.4f && iwMap.listerBuildings.ColonistsHaveBuildingWithPowerOn(ThingDefOf.OrbitalTradeBeacon))
-            {
-                parms.spawnCenter = DropCellFinder.TradeDropSpot(iwMap);
-            }
-            else
-            {
-                RCellFinder.TryFindRandomPawnEntryCell(out parms.spawnCenter, iwMap, 0.0f);
-            }
-        }
-
-        /// <summary>
-        /// Finds the best number of Star Vampires
-        /// to spawn. Then spawns them according
-        /// to a previously resolved location.
-        /// </summary>
-        /// <param name="parms"></param>
-        protected void SpawnStarVampires(IncidentParms parms)
-        {
-            Map iwMap = (Map)parms.target;
-
-            int iwCount = 1;
-
-            if (parms.points <= 2000f)
-            {
-                iwCount = 1;
-            }
-            else if (parms.points <= 3000f)
-            {
-                iwCount = 2;
-            }
-            else if (parms.points <= 5000f)
-            {
-                iwCount = 3;
-            }
-
-            for (int i = 0; i < iwCount; i++)
-            {
-                Thing iwSpawner = ThingMaker.MakeThing(iwDef, null);
-                GenPlace.TryPlaceThing(iwSpawner, iwLoc, iwMap, ThingPlaceMode.Near);
-            //    CosmicHorrorPawn temp = null;
-            //    this.iwVampire = null;
-            //    this.iwSpawner = ThingMaker.MakeThing(this.iwKind, this.iwFac);
-            //    this.iwVampire = this.iwPawn as CosmicHorrorPawn;
-
-                //if (this.lord == null)
-                //{
-                //    LordJob_StarVampire lordJob = new LordJob_StarVampire(this.iwFac, this.iwLoc);
-                //    this.lord = LordMaker.MakeNewLord(this.iwFac, lordJob, iwMap, null);
-                //}
-                //temp = (CosmicHorrorPawn)GenSpawn.Spawn(this.iwVampire, this.iwLoc, iwMap);
-                //this.lord.AddPawn(temp);
-            }
-        }
-    }
+	}
 }
---
+++ Source/CosmicHorrors/MapComponent_StarVampireTracker.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using Verse.AI;
+using Verse.AI.Group;
+
+namespace CosmicHorror
+{
+    public class StarVampireSpawn : IExposable
+    {
+        public enum SpawnerStatus
+        {
+            Countdown = 0,
+            Spawning = 1,
+            MarkedForRemoval = 2
+        }
+
+        public PawnKindDef starVampireSpawnType = null;
+        public Faction starVampireFac = null;
+        public Map mapLocation = null;
+        public IntVec3 positionToSpawn;
+        public int numberToSpawn = 0;
+        public int ticksUntilSpawned = -999;
+        public SpawnerStatus currentStatus = SpawnerStatus.Countdown;
+
+        public bool SafeToRemove() => currentStatus == SpawnerStatus.MarkedForRemoval;
+        
+        public StarVampireSpawn()
+        {
+
+        }
+
+        public StarVampireSpawn(PawnKindDef pkd, Faction fac, Map map, IntVec3 position, int num, int ticks)
+        {
+            starVampireSpawnType = pkd;
+            starVampireFac = fac;
+            mapLocation = map;
+            positionToSpawn = position;
+            numberToSpawn = num;
+            ticksUntilSpawned = ticks;
+            currentStatus = SpawnerStatus.Countdown;
+        }
+
+        public void Tick()
+        {
+            CountDown();
+            ResolveSpawn();
+        }
+
+        public void CountDown()
+        {
+            ticksUntilSpawned--;
+        }
+
+        public void ResolveSpawn()
+        {
+            if (currentStatus == SpawnerStatus.Countdown && ticksUntilSpawned <= 0)
+            {
+                currentStatus = SpawnerStatus.Spawning;
+                TrySpawnPawns();
+            }
+        }
+
+
+        private void TrySpawnPawns()
+        {
+            for(int i = 0; i < numberToSpawn; i++)
+            {
+                int index = this.starVampireSpawnType.lifeStages.Count - 1;
+                Pawn pawn = PawnGenerator.GeneratePawn(new PawnGenerationRequest(this.starVampireSpawnType, this.starVampireFac, PawnGenerationContext.NonPlayer, -1, false, false, false, false, true, false, 1f, false, true, true, true, false, false, false, false, 0f, 0f, null, 1f, null, null, null, null, null, new float?(this.starVampireSpawnType.race.race.lifeStageAges[index].minAge), null, null, null, null, null, null, null, false, false, false));
+                GenSpawn.Spawn(pawn, CellFinder.RandomClosewalkCellNear(this.positionToSpawn, this.mapLocation, 1, null), this.mapLocation, WipeMode.Vanish);
+                Lord lord = mapLocation.GetComponent<MapComponent_StarVampireTracker>().GetStarVampireLord(pawn);
+                lord.AddPawn(pawn);
+            }
+            currentStatus = SpawnerStatus.MarkedForRemoval;
+        }
+
+
+        public void ExposeData()
+        {
+            Scribe_Defs.Look<PawnKindDef>(ref this.starVampireSpawnType, "starVampireSpawnType");
+            Scribe_References.Look<Faction>(ref this.starVampireFac, "starVampireFac");
+            Scribe_References.Look<Map>(ref this.mapLocation, "mapLocation");
+            Scribe_Values.Look(ref this.positionToSpawn, "positionToSpawn");
+            Scribe_Values.Look<int>(ref this.numberToSpawn, "numberToSpawn");
+            Scribe_Values.Look<int>(ref this.ticksUntilSpawned, "ticksUntilSpawned");
+            Scribe_Values.Look(ref this.currentStatus, "currentStatus");
+        }
+
+    }
+
+    public class MapComponent_StarVampireTracker : MapComponent
+    {
+        Lord starVampireLord = null;
+        List<StarVampireSpawn> starVampireSpawners = new List<StarVampireSpawn>();
+
+        public MapComponent_StarVampireTracker(Map map) : base(map)
+        {
+
+        }
+
+        private PawnKindDef GetRandomStarVampireType()
+        {
+            List<PawnKindDef> types = new List<PawnKindDef>
+            {
+                PawnKindDef.Named("ROM_StarVampire"),
+                PawnKindDef.Named("ROM_StarVampireAlbino"),
+                PawnKindDef.Named("ROM_StarVampireAbberation"),
+                PawnKindDef.Named("ROM_StarVampireNight")
+            };
+            return types.RandomElement();
+        }
+
+
+        private Faction GetStarVampireFaction()
+        {
+            return Find.FactionManager.FirstFactionOfDef(FactionDefOf.AncientsHostile);
+        }
+
+
+        public override void MapComponentTick()
+        {
+            base.MapComponentTick();
+            if (starVampireSpawners != null && (starVampireSpawners?.Count() ?? 0) > 0)
+            {
+                foreach (StarVampireSpawn spawner in starVampireSpawners)
+                {
+                    spawner.Tick();
+                }
+                starVampireSpawners.RemoveAll(x => x.SafeToRemove());
+            }
+        }
+
+        public void AddNewStarVampireSpawner(IntVec3 spawnLoc, int num)
+        {
+            if (starVampireSpawners == null) starVampireSpawners = new List<StarVampireSpawn>();
+            IntVec3 intVec;
+            if (!RCellFinder.TryFindRandomPawnEntryCell(out intVec, map, CellFinder.EdgeRoadChance_Animal, false, null))
+            {
+                Log.Error("Cannot find a place to spawn Star Vampire");
+                return;
+            }
+            starVampireSpawners.Add(new StarVampireSpawn(GetRandomStarVampireType(), GetStarVampireFaction(), this.map, intVec, num, 12000));
+        }
+
+        public Lord GetStarVampireLord(Thing caller)
+        {
+            if (starVampireLord == null)
+                starVampireLord = CreateNewLord(caller, true, 99999, typeof(LordJob_AssaultColony));
+            return starVampireLord;
+        }
+
+
+        public static Lord CreateNewLord(Thing byThing, bool aggressive, float defendRadius, Type lordJobType)
+        {
+            IntVec3 invalid;
+            if (!CellFinder.TryFindRandomCellNear(byThing.Position, byThing.Map, 5, (IntVec3 c) => c.Standable(byThing.Map) && byThing.Map.reachability.CanReach(c, byThing, PathEndMode.Touch, TraverseParms.For(TraverseMode.PassDoors, Danger.Deadly, false, false, false)), out invalid, -1))
+            {
+                Log.Error("Found no place for mechanoids to defend " + byThing);
+                invalid = IntVec3.Invalid;
+            }
+            return LordMaker.MakeNewLord(byThing.Faction, Activator.CreateInstance(lordJobType, new object[]
+            {
+                new SpawnedPawnParams
+                {
+                    aggressive = aggressive,
+                    defendRadius = defendRadius,
+                    defSpot = invalid,
+                    spawnerThing = byThing
+                }
+            }) as LordJob, byThing.Map, null);
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Collections.Look<StarVampireSpawn>(ref this.starVampireSpawners, "starVampireSpawners", LookMode.Deep, Array.Empty<object>());
+            Scribe_References.Look<Lord>(ref this.starVampireLord, "starVampireLord");
+        }
+    }
+}
